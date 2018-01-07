bdba3582bda4134dafca1069319610620d9f7da3
Johnny Wong <hin.wong2@yahoo.com.au>
2018-01-07 16:53:46 +1100

    Add count of number of shots taken

--- shooting_drill.py
+++ shooting_drill.py
@@ -8,15 +8,28 @@ import numpy as np
 
 def simulate_drill(lower = -5, upper = 7, perc = 0.7):
 	score = 0
+	shots = 0
 	while (score > lower and score < upper):
 		if np.random.rand() < perc:
 			score += 1
 		else:
 			score -= 2
+		shots += 1
 	
 	if score <= lower:
 		result = 0
 	else:
 		result = 1
 
-	return result
+	return result, shots
+
+'''
+def simulate_drills(n = 1, lower = -5, upper = 7, perc = 0.7):
+	perc = np.array(list(perc)) #TODO 
+	num_won = list(range(perc))
+	for i in range(len(perc)):
+		for j in range(n):
+			num_won[i] += simulate_drill(perc = perc[i])
+		
+	return num_won
+'''
