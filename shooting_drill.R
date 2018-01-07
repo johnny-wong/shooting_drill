4f714b7b83ce9e007ee696f73c74dd18cd701a7f
Johnny Wong <hin.wong2@yahoo.com.au>
2018-01-07 16:09:03 +1100

    Fix error and increase functionality
    Error with simulate drill returning the opposite result as wanted.
    Error testing whether player won or lost the game.
    
    Allows simulate_drills function to take in a vector of shooting percentages.
    Will output a vector corresponding to each percentage

--- shooting_drill.R
+++ shooting_drill.R
@@ -8,21 +8,20 @@ simulate_drill <- function(lower = -5, upper = 7, perc = 0.75){
     }
   }
   
-  if (score == lower){
-    result <- 1
-  } else {
+  if (score <= lower){
     result <- 0
+  } else {
+    result <- 1
   }
   return(result)
 }
 
-#TODO make perc a vector so you can output the vector of results with diff shooting percentages
-
-
 simulate_drills <- function(n = 1, lower = -5, upper = 7, perc = 0.75){
-  num_won <- 0
-  for (i in 1:n){
-    num_won <- num_won + simulate_drill(lower, upper, perc)
+  num_won <- vector(mode = 'numeric', length = length(perc))
+  for (j in 1:length(perc)){
+    for (i in 1:n){
+      num_won[j] <- num_won[j] + simulate_drill(lower, upper, perc[j])
+    }
   }
   return(num_won)
 }
